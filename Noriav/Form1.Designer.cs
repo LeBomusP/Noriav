0301320ef2d87fbbc66b1879ef23c70053daff0f
BombP <84284897+BombPyt@users.noreply.github.com>
2022-03-14 00:19:08 +0100

    Update StartPosition

--- Noriav/Form1.Designer.cs
+++ Noriav/Form1.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(482, 753);
+            this.MaximumSize = new System.Drawing.Size(500, 800);
+            this.MinimumSize = new System.Drawing.Size(500, 800);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
